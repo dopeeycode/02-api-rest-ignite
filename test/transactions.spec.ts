1f67711c19f3aedfc33dd07e05900807f682c558
dopeey <italodopeey@gmail.com>
2025-08-24 10:42:53 -0300

    test: should be able to list all transactions

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,4 +1,4 @@
-import { beforeAll, afterAll, describe, it } from 'vitest'
+import { beforeAll, afterAll, describe, it, expect } from 'vitest'
 import request from 'supertest'
 import { app } from '../src/app'
 
@@ -11,6 +11,10 @@ describe('Transactions routes', () => {
     await app.close()
   })
 
+  // Todo teste ele deve obrigatóriamente se excluir de qualquer contexto
+  // Jamais devo escrever um teste que dependa de outro teste
+  // Se o teste depende de outro ele deveria ser apenas um
+
   it('should be able to create a new transaction', async () => {
     await request(app.server)
       .post('/transactions')
@@ -20,5 +24,29 @@ describe('Transactions routes', () => {
         type: 'credit',
       })
       .expect(201)
+  })
+
+  it('should be able to list all transactions', async () => {
+    const createTransactionResponse = await request(app.server)
+      .post('/transactions')
+      .send({
+        title: 'new Transaction',
+        amount: 5000,
+        type: 'credit',
+      })
+
+    const cookies = createTransactionResponse.get('Set-Cookie') || []
+
+    const listTransactionsResponse = await request(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+      .expect(200)
+
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: 'new Transaction',
+        amount: 5000,
+      }),
+    ])
   })
 })
